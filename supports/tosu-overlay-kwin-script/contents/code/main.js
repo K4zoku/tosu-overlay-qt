046ad189d111bf3a92101e40cd6e355d4047118e
Kazoku <k4zoku@pm.me>
2025-10-17 21:14:57 +0700

    fix: revert changes

--- supports/tosu-overlay-kwin-script/contents/code/main.js
+++ supports/tosu-overlay-kwin-script/contents/code/main.js
@@ -1,48 +1,30 @@
-let cachedGeometry = { x: 0, y: 0, width: 0, height: 0 };
-
 function sendGeometry(geometry) {
-    if (isGeometryEqual(geometry, cachedGeometry)) {
-        return;
-    }
-    cachedGeometry = geometry;
     callDBus("app.tosu.Overlay", "/", "app.tosu.Overlay.Ipc", "geometryChanged",
         geometry.x, geometry.y, geometry.width, geometry.height);
 }
 
-function isGeometryEqual(geometry1, geometry2) {
-    return geometry1.x === geometry2.x &&
-        geometry1.y === geometry2.y &&
-        geometry1.width === geometry2.width &&
-        geometry1.height === geometry2.height;
-}
+function connectWindowGeometryChanged(window) {
+    sendGeometry(window.clientGeometry);
 
-const osuResourceNames = [
-    "osu!", // osu!lazer
-    "osu!.exe", // osu!stable
-];
-
-function isOsuWindow(window) {
-    return osuResourceNames.includes(window.resourceName());
-}
-
-// Scan for currently opened osu window
-for (let window of workspace.windowList()) {
-    if (isOsuWindow()) {
+    window.clientGeometryChanged.connect(function(oldGeometry) {
         sendGeometry(window.clientGeometry);
-        window.clientGeometryChanged.connect(() => sendGeometry(window.clientGeometry));
-    }
+    });
 }
 
-// Connect event for new osu window added
 workspace.windowAdded.connect(function(window) {
-    if (isOsuWindow(window)) {
-        sendGeometry(window.clientGeometry);
-        window.clientGeometryChanged.connect(() => sendGeometry(window.clientGeometry));
+    if (window.resourceName === "osu!.exe" || window.resourceName === "osu!") {
+        connectWindowGeometryChanged(window);
     }
 });
 
 workspace.windowActivated.connect(function(window) {
-    if (isOsuWindow(window)) {
+    if (window.resourceName === "osu!.exe" || window.resourceName === "osu!") {
         sendGeometry(window.clientGeometry);
     }
 });
+
+for (let window of workspace.windowList()) {
+    if (window.resourceName === "osu!.exe" || window.resourceName === "osu!") {
+        connectWindowGeometryChanged(window);
+    }
+}
