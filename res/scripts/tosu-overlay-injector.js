e3b37d0da4acfb847eee9ebe669990f0fc96a644
Kazoku <k4zoku@pm.me>
2025-10-29 16:28:34 +0700

    style(tosu): format overlay injector script

--- res/scripts/tosu-overlay-injector.js
+++ res/scripts/tosu-overlay-injector.js
@@ -1,10 +1,15 @@
 (() => {
-  let script = document.createElement('script');
-  script.onload = function() {
-    let object = null;
-    new QWebChannel(qt.webChannelTransport, (channel) => object = channel.objects.object);
-    document.addEventListener('keydown', (event) => object?.onKeyDown(event.key));
+  const script = document.createElement("script");
+  script.onload = function () {
+    let object;
+    new QWebChannel(
+      qt.webChannelTransport,
+      (channel) => (object = channel.objects.object),
+    );
+    document.addEventListener("keydown", (event) =>
+      object?.onKeyDown(event.key),
+    );
   };
-  script.src = 'qrc:///qtwebchannel/qwebchannel.js';
+  script.src = "qrc:///qtwebchannel/qwebchannel.js";
   document.body.appendChild(script);
 })();
